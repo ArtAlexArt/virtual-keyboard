84b77102fabf1b0d8e848b087734cd86d3bec6bd
Alexander Artyomchenko <art_alex@ukr.net>
2022-05-09 18:30:55 +0300

    feat: index.js add function for keys

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -93,8 +93,21 @@ let lang = localStorage.getItem('lang') || 'en';
 let curPos = 0;
 let curPosNew = curPos;
 
+const setSelectionRange = (input, selectionStart, selectionEnd) => {
+  //   if (input.setSelectionRange) {
+  input.focus();
+  input.setSelectionRange(selectionStart, selectionEnd);
+  //   }
+  // else if (input.createTextRange) {
+  //     const range = input.createTextRange();
+  //     range.collapse( true );
+  //     range.moveEnd( 'character', selectionEnd );
+  //     range.moveStart( 'character', selectionStart );
+  //     range.select();
+  // }
+};
 
-
+const setCaretToPos = (input, pos) => setSelectionRange(input, pos, pos);
 
 const keyboard = {
   elements: {
@@ -150,8 +163,282 @@ const keyboard = {
     document.body.appendChild(this.elements.main);
   },
 
- };
+  createKeys() {
+    const fragment = document.createDocumentFragment();
 
+    for (let i = 0; i < keys.length; i += 1) {
+      const divRow = document.createElement('div');
+      divRow.classList.add('keyboard__row', 'row');
+
+      for (const key in keys[i]) {
+        const div = document.createElement('div');
+        div.classList.add('keyboard__key', 'key', key, keys[i][key][2]);
+
+        const butRus = document.createElement('span');
+        butRus.classList.add('rus');
+
+        const butEng = document.createElement('span');
+        butEng.classList.add('eng');
+
+        sp.map((el, ind) => {
+          const sp1 = document.createElement('span');
+          sp1.innerText = keys[i][key][0][ind];
+          sp1.classList.add(el, 'hidden');
+          sp1.addEventListener('click', (e) => this.keyMouseClick(e));
+          butRus.appendChild(sp1);
+          const sp2 = document.createElement('span');
+          sp2.innerText = keys[i][key][1][ind];
+          sp2.classList.add(el, 'hidden');
+          sp2.addEventListener('click', (e) => this.keyMouseClick(e));
+          butEng.appendChild(sp2);
+          return { butRus, butEng };
+        });
+
+        lang === 'en' ? butEng.children[0].classList.remove('hidden') : butRus.children[0].classList.remove('hidden');
+
+        div.appendChild(butRus);
+        div.appendChild(butEng);
+        divRow.appendChild(div);
+      }
+      fragment.appendChild(divRow);
+    }
+    return fragment;
+  },
+
+  changeHidden(indF) {
+    const ind = lang === 'en' ? 1 : 0;
+    this.elements.keys.forEach((el) => {
+      const elt = el.children[ind];
+      for (let i = 0; i < elt.childElementCount; i += 1) elt.children[i].classList.add('hidden');
+      if (indF > -1) elt.children[indF].classList.remove('hidden');
+    });
+  },
+
+  keyMouseClick(e) {
+    const butKey = e.target;
+    const key = butKey.outerText;
+    const keyCode = Number(e.target.parentElement.parentElement.classList[3]);
+    const code = e.target.parentElement.parentElement.classList[2];
+    let evt = null;
+    keyCode === 16
+      ? evt = new KeyboardEvent('keydown', {
+        keyCode, which: keyCode, code, key, shiftKey: true
+      })
+      : evt = new KeyboardEvent('keydown', {
+        keyCode, which: keyCode, code, key
+      });
+    document.dispatchEvent(evt);
+
+    setTimeout(() => {
+      evt = new KeyboardEvent('keyup', {
+        keyCode, which: keyCode, code, key
+      });
+      document.dispatchEvent(evt);
+    }, 350);
+  }
+};
+
+let ctrl = false;
+document.addEventListener('keydown', (e) => {
+  let sound = 3;
+  const codekey = e.code;
+  let { key } = e;
+
+  if (codekey === 'ControlLeft') {
+    key = '';
+    ctrl = true;
+  }
+
+  if (codekey === 'AltLeft' || codekey === 'AltRight') {
+    key = '';
+    if (ctrl === true) {
+      keyboard.changeHidden(-1);
+      lang = lang === 'en' ? 'ru' : 'en';
+      localStorage.setItem('lang', lang);
+      keyboard.properties.capsLock
+        ? keyboard.changeHidden(2)
+        : keyboard.changeHidden(0);
+      ctrl = false;
+    }
+  }
+
+  e.preventDefault();
+  const butKey = document.querySelector(`.${codekey}`);
+  // console.log(butKey)
+  if (butKey) {
+    if (key) key = butKey.innerText;
+    butKey.style.cssText = 'transform: translate(3px, 3px);border-radius:17px; background-color:  #b7e10f;';
+    butKey.click();
+  }
+
+  if (codekey === 'CapsLock') {
+    key = '';
+    sound = 4;
+    if (keyboard.properties.capsLock) {
+      butKey.classList.remove('active');
+      keyboard.properties.capsLock = false;
+    } else {
+      butKey.classList.add('active');
+      keyboard.properties.capsLock = true;
+    }
+    keyboard.properties.capsLock
+      ? keyboard.properties.shift
+        ? keyboard.changeHidden(3)
+        : keyboard.changeHidden(2)
+      : keyboard.changeHidden(0);
+  }
+
+  if (['ShiftLeft', 'ShiftRight', 'Shift'].includes(codekey)) {
+    key = '';
+    sound = 4;
+    if (!keyboard.properties.shift) {
+      butKey.classList.add('active');
+      keyboard.properties.shift = true;
+    }
+    keyboard.properties.capsLock ? keyboard.changeHidden(3) : keyboard.changeHidden(1);
+  }
+
+  if (['Backspace', 'Delete', 'ControlRight', 'AltLeft', 'AltRight', 'MetaLeft', 'Tab', 'AltRight'].includes(codekey)) {
+    key = '';
+    sound = 7;
+  }
+
+  const { textarea } = keyboard.elements;
+
+  switch (codekey) {
+    case 'Backspace':
+      myAudio[sound].currentTime = 0;
+      myAudio[sound].play();
+      curPos = textarea.selectionStart;
+      if (curPos !== 0) {
+        if (curPos === textarea.value.length && curPos !== 0) {
+          textarea.value = textarea.value.substring(0, textarea.value.length - 1);
+          setCaretToPos(textarea, curPos - 1);
+        } else {
+          const cache = textarea.value.slice(curPos);
+          textarea.value = textarea.value.slice(0, curPos)
+            .substring(0, textarea.value.slice(0, curPos).length - 1) + cache;
+          setCaretToPos(textarea, curPos - 1);
+        }
+      }
+      textarea.focus();
+      break;
+    case 'Delete':
+      myAudio[sound].currentTime = 0;
+      myAudio[sound].play();
+      curPos = textarea.selectionStart;
+      if (curPos !== textarea.value.length) {
+        const cache = textarea.value.slice(curPos + 1);
+        textarea.value = textarea.value.slice(0, curPos)
+          .substring(0, textarea.value.slice(0, curPos).length + 1) + cache;
+        setCaretToPos(textarea, curPos);
+      }
+      textarea.focus();
+      break;
+
+    case 'ArrowLeft':
+      key = '';
+      sound = 7;
+      myAudio[sound].currentTime = 0;
+      myAudio[sound].play();
+      curPos = textarea.selectionStart;
+      if (curPos !== 0) setCaretToPos(textarea, curPos - 1);
+      textarea.focus();
+      break;
+    case 'ArrowRight':
+      key = '';
+      sound = 7;
+      myAudio[sound].currentTime = 0;
+      myAudio[sound].play();
+      curPos = textarea.selectionStart;
+      if (curPos !== textarea.value.length) setCaretToPos(textarea, curPos + 1);
+      textarea.focus();
+      break;
+    case 'ArrowUp':
+      key = '';
+      sound = 7;
+      myAudio[sound].currentTime = 0;
+      myAudio[sound].play();
+      curPos = textarea.selectionStart;
+      curPosNew = curPos;
+      if (textarea.value.length) {
+        const rows = textarea.value.split('\n');
+        let i = 0;
+        curPosNew = 0;
+        while (i < rows.length) {
+          if (curPos > curPosNew + rows[i].length + 1) curPosNew += rows[i].length + 1;
+          else {
+            i ? (curPos - curPosNew < rows[i - 1].length)
+              ? curPosNew = curPos - 1 - rows[i - 1].length
+              : curPosNew -= 1 : curPosNew = curPos - rows[0].length - 1;
+            i = rows.length;
+          }
+          i += 1;
+        }
+        setCaretToPos(textarea, curPosNew);
+      }
+      textarea.focus();
+      break;
+    case 'ArrowDown':
+      key = '';
+      sound = 7;
+      myAudio[sound].currentTime = 0;
+      myAudio[sound].play();
+      curPos = textarea.selectionStart;
+      curPosNew = curPos;
+      if (textarea.value.length) {
+        const rows = textarea.value.split('\n');
+        let i = 0;
+        curPosNew = 0;
+        while (i < rows.length) {
+          if (curPos >= curPosNew + rows[i].length + 1) curPosNew += rows[i].length + 1;
+          else {
+            (i + 1) < rows.length
+              ? (curPos - curPosNew <= rows[i + 1].length)
+                ? curPosNew = curPos + rows[i].length + 1
+                : curPosNew += rows[i].length + rows[i + 1].length + 1
+              : curPosNew = curPos + rows[i].length;
+            i = rows.length;
+          }
+          i += 1;
+        }
+        setCaretToPos(textarea, curPosNew);
+      }
+      textarea.focus();
+      break;
+    default:
+      if (codekey === 'Tab') key = '\t';
+      if (codekey === 'Space') key = ' ';
+      if (codekey === 'Enter') key = '\n';
+      myAudio[sound].currentTime = 0;
+      myAudio[sound].play();
+      curPos = textarea.selectionStart;
+      if (curPos === textarea.value.length) {
+        textarea.value += key;
+      } else {
+        const cache = textarea.value.slice(curPos);
+        textarea.value = textarea.value.slice(0, curPos) + key + cache;
+      }
+      textarea.focus();
+      if (key) setCaretToPos(textarea, curPos + 1);
+      break;
+  }
+});
+
+document.addEventListener('keyup', (e) => {
+  const butKey = document.querySelector(`.${e.code}`);
+  butKey.style.cssText = 'transform: translate(0px, 0px)';
+
+  if (e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
+    if (keyboard.properties.shift) {
+      butKey.classList.remove('active');
+      keyboard.properties.shift = false;
+    }
+    keyboard.properties.capsLock
+      ? keyboard.changeHidden(2)
+      : keyboard.changeHidden(0);
+  }
+});
 
 window.addEventListener('DOMContentLoaded', () => {
   keyboard.init();
